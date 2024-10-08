2e0a47b1d0c9975074e3ac93140df8476725827a
ZitouneMcGregor <carreregee.olivier@gmail.com>
2024-10-08 14:50:21 +0200

    routing de base base base

--- futfutfut/front/src/app/app.component.ts
+++ futfutfut/front/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
